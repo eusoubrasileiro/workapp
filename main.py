2fc98cc32a54d761f7d2833d0be084b2f19aef79
eusoubrasileiro <eusoubrasileiro@gmail.com>
2022-09-01 21:43:31 -0300

    interferencia app updates - using all columns legacy excel - sending post event checkbox prioridade - fixed some bugs - rewrite for less flask routes - ready to write to file each /save post

--- main.py
+++ main.py
@@ -8,46 +8,60 @@ import os, sys
 import pathlib  
 import argparse
 import pandas as pd
+from threading import Thread
 import xml.etree.ElementTree as etree
 
 from flask_caching import Cache
-from flask import Flask, render_template, request, redirect
+from flask import (
+    Flask, render_template, request, redirect, Response
+    )
 from .....web.pandas_html import dataframe_to_html 
 from ... import workflows as wf 
 from ...scm.util import fmtPname
 
 curpath = os.path.dirname(os.path.abspath(__file__))
 
+
 def readExcelLegacy(path):
     excel_path = pathlib.Path(path).glob('eventos_prioridade_*.xlsx')
     if not excel_path: 
         raise RuntimeError("Legacy Excel prioridade not found, something like eventos_prioridade_*.xlsx")
-    table = pd.read_excel(list(excel_path)[0])
-    cols = ['Ativo', 'Processo', 'Evento', 'EvSeq', 'Descrição', 'Data', 'Obs', 'DOU', 'Dads', 'Sons']
-    table = table[cols]
-    table.fillna('-', inplace=True) # fill nan to -
+    table = pd.read_excel(list(excel_path)[0])    
+    table.fillna('', inplace=True) # fill nan to -
     # additional columns created for stilying or UI/UX with css, jquery, jscript
-    table['group'] = table.groupby(table.Processo).ngroup() # set index as id of process 
-    table['evindex'] = table.groupby(table.Processo).cumcount() # number of each item inside each group
-    # evn is givin unordered number of events to be added as attribute
-    table['evn'] = [ p for v in table.groupby(table.Processo).size() for p in [v]*v ] # number of events
-    print(table, file=sys.stderr)
-    print(table['evn'], file=sys.stderr)
+    table['group'] = table.Processo # which process group it belongs
+    table['evindex'] = None 
+    table['evn'] = None 
+    for name, group in table.groupby(table.Processo):
+        table.loc[group.index, 'evn'] = len(group) 
+        table.loc[group.index, 'evindex'] = list(range(len(group)))
+        # unecessary information - making visual analysis too poluted
+        table.loc[group.index[1:], 'Ativo'] = '' 
+        table.loc[group.index[1:], 'Prior'] = ''  # 1'st will be replaced by a checkbox
+        table.loc[group.index[1:], 'Processo'] = ''
+        table.loc[group.index[1:], 'Dads'] = ''
+        table.loc[group.index[1:], 'Sons'] = ''
+    cols = ['Prior', 'Ativo', 'Processo', 'Evento', 'EvSeq', 'Descrição', 'Data', 
+            'DataPrior', 'EvPrior', 'Inativ', 'Obs', 'DOU', 'Dads', 'Sons',
+            'group', 'evindex', 'evn']
+    table = table[cols]
     row_attrs = ['Ativo', 'group', 'evindex', 'evn'] # List of columns to write as attributes in row element.
     row_cols = table.columns.to_list() # List of columns to write as children in row element. By default, all columns
-    row_cols = [ v for v in row_cols if v not in ['group', 'evindex', 'evn'] ]
+    row_cols = [ v for v in row_cols if v not in ['group', 'evindex', 'evn'] ]        
     html_table = dataframe_to_html(table, row_attrs, row_cols)
     # insert checkboxes on first row of each process for prioridade
-    for e in html_table.iterfind(".//tbody/tr[@evindex='0']/td[1]"):
-        if 'Sim' in e.text:
-            e.text = etree.SubElement(e, "input", { 'type': 'checkbox', 'checked': 'true'}) 
+    # add span around everything except this checkbox for clicking porpouses
+    for main_row in html_table.findall(".//tbody/tr[@evindex='0']"):    
+        children = list(main_row)                     
+        if '1' in children[0].text:            
+            etree.SubElement(children[0], "input", { 'type': 'checkbox', 'checked': 'true'}) 
         else:
-            e.text = etree.SubElement(e, "input", { 'type': 'checkbox'}) 
-    #[ e.set('text', '') for e in html_table.iterfind(".//tbody/tr[not(@evindex='0')]/td[1]")]
-    html_table = etree.tostring(html_table, encoding='unicode', method='xml')   
+            etree.SubElement(children[0], "input", { 'type': 'checkbox'})  
+        children[0].text = ''
+    html_table = etree.tostring(html_table, encoding='unicode', method='xml')       
     return html_table
 
-def chooseProcess():
+def getProcessesAvailable():
     wf.currentProcessGet()
     return wf.ProcessPathStorage
     
@@ -55,7 +69,7 @@ app = Flask(__name__, template_folder=curpath)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 30*60 # 30 mins in seconds
 app.config['CACHE_TYPE'] = 'SimpleCache' 
 cache = Cache(app)
-cache.set('processos_list', chooseProcess())
+cache.set('processos_list', getProcessesAvailable())
 cache.set('selected', None)
 
 @app.route('/')
@@ -63,20 +77,22 @@ def chooseProcess():
     return render_template('index.html', 
                 processos_list=cache.get('processos_list'))
 
-@app.route('/table')
-def table():
-    if cache.get('selected'):
-        key = cache.get('selected')        
-        return render_template('index.html', 
-                               pandas_table=readExcelLegacy(wf.ProcessPathStorage[key]))
-    return redirect("/", code=302)        
-        
 @app.route('/select', methods=['GET'])
 def select():    
     cache.set('selected', fmtPname(request.args.get('selected')))
-    return redirect("/table", code=302)        
+    key = cache.get('selected')        
+    return render_template('index.html', processo=key,
+                    pandas_table=readExcelLegacy(wf.ProcessPathStorage[key]))    
     
-    
+@app.route('/save', methods=['POST'])
+def save():
+    data = request.get_json()    
+    Thread(target=updateFile, args=(data['process'], data['state'])).start()
+    return Response(status=204)
+
+def updateFile(processo, state):
+    pass
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-d','--debug', default=True, action='store_true')    
