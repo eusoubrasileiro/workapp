b096d56030b6865082a4fc8ff4269d4e6717ce03
eusoubrasileiro <eusoubrasileiro@gmail.com>
2022-09-04 14:43:29 -0300

    interferencia app updating on disk - saving interferencia table and states on disk using json

--- main.py
+++ main.py
@@ -4,73 +4,75 @@ and save it's 'prioridade' results direct from browser.add()
 Run from ~/Projects 
 python -m aidbag.anm.careas.estudos.app.main
 """
-import os, sys
-import pathlib  
+from inspect import trace
+import os, sys 
+import pandas as pd 
 import argparse
-import pandas as pd
+import pathlib
+import traceback
 from threading import Thread
 import xml.etree.ElementTree as etree
 
 from flask_caching import Cache
 from flask import (
-    Flask, render_template, request, redirect, Response
+        make_response,
+        Flask, 
+        render_template, 
+        request, 
+        Response
     )
-from .....web.pandas_html import dataframe_to_html 
+
+from ...config import config
 from ... import workflows as wf 
-from ...scm.util import fmtPname
+from .....web.pandas_html import dataframe_to_html 
+from ...scm.util import fmtPname, numberyearPname
+from ...estudos.interferencia import (
+        Interferencia, 
+        prettyTabelaInterferenciaMaster
+    )
 
 curpath = os.path.dirname(os.path.abspath(__file__))
 
 
-def readExcelLegacy(path):
-    excel_path = pathlib.Path(path).glob('eventos_prioridade_*.xlsx')
-    if not excel_path: 
-        raise RuntimeError("Legacy Excel prioridade not found, something like eventos_prioridade_*.xlsx")
-    table = pd.read_excel(list(excel_path)[0])    
-    table.fillna('', inplace=True) # fill nan to -
+def htmlTable(table): 
+    """crate a html code for a pretty view of dataframe `table` """
     # additional columns created for stilying or UI/UX with css, jquery, jscript
+    table = table.copy()
     table['group'] = table.Processo # which process group it belongs
     table['evindex'] = None 
     table['evn'] = None 
     for name, group in table.groupby(table.Processo):
         table.loc[group.index, 'evn'] = len(group) 
         table.loc[group.index, 'evindex'] = list(range(len(group)))
-        # unecessary information - making visual analysis too poluted
-        table.loc[group.index[1:], 'Ativo'] = '' 
-        table.loc[group.index[1:], 'Prior'] = ''  # 1'st will be replaced by a checkbox
-        table.loc[group.index[1:], 'Processo'] = ''
-        table.loc[group.index[1:], 'Dads'] = ''
-        table.loc[group.index[1:], 'Sons'] = ''
-    cols = ['Prior', 'Ativo', 'Processo', 'Evento', 'EvSeq', 'Descrição', 'Data', 
+    table = prettyTabelaInterferenciaMaster(table, view=True)  # some prettify 
+    # for backward compatibility rearrange columns
+    table = table[['Prior', 'Ativo', 'Processo', 'Evento', 'EvSeq', 'Descrição', 'Data', 
             'DataPrior', 'EvPrior', 'Inativ', 'Obs', 'DOU', 'Dads', 'Sons',
-            'group', 'evindex', 'evn']
-    table = table[cols]
+            'group', 'evindex', 'evn']] 
     row_attrs = ['Ativo', 'group', 'evindex', 'evn'] # List of columns to write as attributes in row element.
     row_cols = table.columns.to_list() # List of columns to write as children in row element. By default, all columns
     row_cols = [ v for v in row_cols if v not in ['group', 'evindex', 'evn'] ]        
     html_table = dataframe_to_html(table, row_attrs, row_cols)
-    # insert checkboxes on first row of each process for prioridade
-    # add span around everything except this checkbox for clicking porpouses
-    for main_row in html_table.findall(".//tbody/tr[@evindex='0']"):    
-        children = list(main_row)                     
-        if '1' in children[0].text:            
-            etree.SubElement(children[0], "input", { 'type': 'checkbox', 'checked': 'true'}) 
+    # insert checkboxes on first row of each process 'Prior' column for click-check prioridade
+    for main_row in html_table.findall(".//tbody/tr[@evindex='0']"):                    
+        if '1' in main_row[0].text:            
+            etree.SubElement(main_row[0], "input", { 'type': 'checkbox', 'checked': 'true'}) 
         else:
-            etree.SubElement(children[0], "input", { 'type': 'checkbox'})  
-        children[0].text = ''
+            etree.SubElement(main_row[0], "input", { 'type': 'checkbox'})  
+        main_row[0].text = ''   
     html_table = etree.tostring(html_table, encoding='unicode', method='xml')       
-    return html_table
+    return html_table   
 
-def getProcessesAvailable():
-    wf.currentProcessGet()
-    return wf.ProcessPathStorage
-    
+   
 app = Flask(__name__, template_folder=curpath)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 30*60 # 30 mins in seconds
-app.config['CACHE_TYPE'] = 'SimpleCache' 
+app.config['CACHE_TYPE'] = 'SimpleCache'  # only 1 thread connection
 cache = Cache(app)
-cache.set('processos_list', getProcessesAvailable())
+cache.set('processos_list', wf.currentProcessGet())
 cache.set('selected', None)
+cache.set('table', None)
+cache.set('json_path', None)
+
 
 @app.route('/')
 def chooseProcess():    
@@ -80,18 +82,41 @@ def chooseProcess():
 @app.route('/select', methods=['GET'])
 def select():    
     cache.set('selected', fmtPname(request.args.get('selected')))
-    key = cache.get('selected')        
-    return render_template('index.html', processo=key,
-                    pandas_table=readExcelLegacy(wf.ProcessPathStorage[key]))    
+    key = cache.get('selected')
+    number, year = numberyearPname(key)
+    json_path = (pathlib.Path(wf.ProcessPathStorage[key]) / (config['interferencia']['file_prefix'] 
+                 + '_' + '_'.join([number, year])+'.json'))          
+    cache.set('json_path', json_path)
+    try: # json first         
+        table = pd.read_json(json_path)
+    except Exception as e: # legacy then        
+        print("Not using local json!", file=sys.stderr)
+        estudo = Interferencia.from_excel(wf.ProcessPathStorage[key])
+        table = estudo.tabela_interf_master
+    cache.set('table', table)
+    response = make_response(render_template('index.html', processo=key,
+                pandas_table=htmlTable(table)) )
+    response.headers['X-Parachutes'] = 'parachutes are cool'
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
     
-@app.route('/save', methods=['POST'])
-def save():
+@app.route('/update', methods=['POST'])
+def update():
     data = request.get_json()    
-    Thread(target=updateFile, args=(data['process'], data['state'])).start()
+    Thread(target=save, args=(data['process'], data['state'])).start()
     return Response(status=204)
 
-def updateFile(processo, state):
-    pass
+def save(processo, state):    
+    """update cached estudo table and estudo file on disk"""    
+    table = cache.get('table')      
+    if table is not None:  
+        table.loc[table.Processo == processo, 'Prior'] = '1' if state else '0'    
+        table = prettyTabelaInterferenciaMaster(table, view=False)
+        with open(cache.get('json_path'), 'w') as f:
+            table.to_json(f)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
