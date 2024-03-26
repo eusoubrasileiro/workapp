5d28aa9c22a7df3956c99b0088ebaf38e0d57058
eusoubrasileiro <eusoubrasileiro@gmail.com>
2024-03-26 20:52:22 -0300

    workapp run opens brower(chrome) on localhost:port

--- run.py
+++ run.py
@@ -1,13 +1,24 @@
 import argparse
 import os
+import time 
+import subprocess
+import threading
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run the workapp app or only backend in development mode -d or --dev')
     parser.add_argument('-d', '--dev', action='store_true', help='Run in development mode', default=False)
     args = parser.parse_args()
-    if args.dev:         # frontend being served from nodejs `npm start`
+    if args.dev: # frontend being served from nodejs `npm start`
         os.environ['APP_ENV'] = 'development'
+        port = 3000
     else:
         os.environ['APP_ENV'] = 'production'
+        port = 5000
     from . import main # can only import after APP_ENV is set
-    main.run()  # Call the original main function
+    # Execute the command in the shell
+    def open_browser():
+        time.sleep(1.5)
+        subprocess.run(f"google-chrome http://localhost:{port}", shell=True)
+    threading.Thread(target=open_browser).start()    
+    main.run()  # Call the original main function    
+    
